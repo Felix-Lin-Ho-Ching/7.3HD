5c777e900c708103ab4eecf365f005ed4bd989d4
Ho Ching Lin <felixlinhoching@gmail.com>
2025-09-27 13:14:28 +1000

    update

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,8 +1,12 @@
 export default {
-    testEnvironment: 'node',
+    testEnvironment: "node",
     transform: {},
+    testMatch: ["**/tests/**/*.test.js", "**/src/**/__tests__/**/*.test.js"],
     reporters: [
-        'default',
-        ['jest-junit', { outputDirectory: './reports/junit', outputName: 'junit.xml' }],
+        "default",
+        ["jest-junit", { outputDirectory: "reports/junit", outputName: "junit.xml" }]
     ],
+    detectOpenHandles: true,
+    forceExit: true,
+    testTimeout: 20000
 };
